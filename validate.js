c8013eacdfdb42eacd4c4ab04e9b422be2119c48
nicolsek <tusek.nicole@gmail.com>
2018-04-10 10:32:03 -0700

    Added basic object creation and validation.

--- validate.js
+++ validate.js
@@ -3,10 +3,12 @@
 /* charWrapper ... if a char is entered it returns the charcode vice versa with charcode. */
 function charWrapper(val) {
     var newVal;
-    
-    if (typeof(val) == int) 
-        newVal = String.fromCharCode(val); 
-    } else {
+
+    if (typeof(val) == typeof(0)) {
+        newVal = String.fromCharCode(val);
+    }
+
+    if (typeof(val) == typeof("")) {
         newVal = val.charCodeAt(0);
     }
 
@@ -21,27 +23,77 @@ function generateChars(start, end) {
     characters = [];
 
     for (newStart; newStart <= newEnd; ++newStart) {
-        characters.append(charWrapper(newStart));
+        characters.push(charWrapper(newStart));
     }
+
+    return characters
 }
 
 /* createValidator ... returns a validator object of the customized type. */
-function createValidator(name, allowed) {
-    /** Characters ... the characters that are allowed in the text. 
+function createValidator(config) {
+    /** Characters ... the characters that are allowed in the text.
         Options ... "alpha", "Alpha", "ALPHA", lower-case, mixed-case, upper-case, respectively.
         "numeric", all numbers 0 -> 9, "special", all special characters. Separate each with hyphen.
-    */ 
+    */
+
+    characters = config.characters;
+
+    var allowedCharacters = [];
 
     characters.split("-").forEach(function(option) {
         switch(option) {
             case "alpha":
+                allowedCharacters.push(generateChars("a", "z"));
+                break;
             case "Alpha":
+                allowedCharacters.push(generateChars("A", "z"));
+                break;
             case "ALPHA":
-            
+                allowedCharacters.push(generateChars("A", "Z"));
+                break;
             case "numeric":
+                allowedCharacters.push(generateChars("1", "9"));
+                break;
             case "special":
+                allowedCharacters.push(generateChars("!", "/"));
+                allowedCharacters.push(generateChars(":", "@"));
+                allowedCharacters.push(generateChars("[", "`"));
+                allowedCharacters.push(generateChars("{", "~"));
+                break;
         }
     });
+
+    this.allowedCharacters = [].concat.apply([], allowedCharacters);
+
+    this.minLength = config.minLength;
+    this.maxLength = config.maxLength;
+
+    this.mustHave = config.mustHave;
+    var mustContain = [];
+
+    switch(config.mustContain) {
+        case "special":
+            mustContain.push(generateChars("!", "/"));
+            mustContain.push(generateChars(":", "@"));
+            mustContain.push(generateChars("[", "`"));
+            mustContain.push(generateChars("{", "~"));
+            break;
+    }
+
+    this.mustContain = [].concat.apply([], mustContain);
+
+    this.banned = config.banned;
+    this.bannedContain = config.bannedContain;
+
+    this.validate = function(toValidate) {
+        if (typeof(toValidate) == typeof([])) {
+
+        } else {
+
+        }
+    }
+
+    return this;
 }
 
 /* Example */
@@ -51,11 +103,11 @@ var allowed = {
     minLength: 4,
     maxLength: 16,
 
-    mustHave = 2,
-    mustContain = "special", /* Text must contain mustHave number of special characters  */
+    mustHave: 2,
+    mustContain: "special", /* Text must contain mustHave number of special characters  */
 
-    banned = ["password", "password123"], /* Cannot be these exact phrases */
-    bannedContain = ["password*", "*1234", "*pizza*"] /* Cannot contain these phrases, use wildcard to include anything after or before.
+    banned: ["password", "password123"], /* Cannot be these exact phrases */
+    bannedContain: ["password*", "*1234", "*pizza*"] /* Cannot contain these phrases, use wildcard to include anything after or before. */
 }
 
 passwordValidator = createValidator(allowed);
